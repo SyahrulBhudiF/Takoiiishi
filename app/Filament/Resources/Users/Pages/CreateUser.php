6478367a68e253e6c6405dda6d12a600624203b9
ryuko <syahrul4w@gmail.com>
2026-06-03 08:15:28 +0700

    fix: Align with spatie

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use BackedEnum;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
@@ -10,6 +11,13 @@ class CreateUser extends CreateRecord
     protected static string $resource = UserResource::class;
 
     protected static bool $canCreateAnother = false;
+
+    protected function afterCreate(): void
+    {
+        $role = $this->record->role;
+
+        $this->record->syncRoles([$role instanceof BackedEnum ? $role->value : $role]);
+    }
 
     protected function getRedirectUrl(): string
     {
